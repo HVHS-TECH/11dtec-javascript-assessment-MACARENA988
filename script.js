1e2103b74fc4b62fd5d73350eebe96d6efcb29a2
Jacob Williams <24092jw@hvhs.school.nz>
2026-06-04 15:15:04 +1200

    Order

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+console.log("script.js")
 console.log("Hello world!")
+
+//Varibles
+
+/****************************
+Main code:
+****************************/
+
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+/****************************
+Functions:
+****************************/
+
+
+
+function getFormInput() {
+    const ORDER_FIELD = document.getElementById("orderField");
+    let userOrder = Number(ORDER_FIELD_FIELD.value);
+    OUTPUT.innerHTML = "<p>You have " + userOrder + ".</p>";
+  
+   
+  }
