24e59eff58cae3e4beb08ee60de329bd1e32e60a
Jacob Williams <24092jw@hvhs.school.nz>
2026-06-08 09:22:58 +1200

    8

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Hello world!")
 
 //Varibles
 
+let userOrder = ["Eggs", "Steak", "Avocado", "Raw milk", "Banana", "Cheese"];
 /****************************
 Main code:
 ****************************/
@@ -17,8 +18,9 @@ Functions:
 
 function getFormInput() {
     const ORDER_FIELD = document.getElementById("orderField");
-    let userOrder = Number(ORDER_FIELD_FIELD.value);
-    OUTPUT.innerHTML = "<p>You have " + userOrder + ".</p>";
+    let userOrder = Number(ORDER_FIELD.value);
+
+    OUTPUT.innerHTML = "1. "+ userOrder[userOrder]+ "2. "<br>";
   
    
   }
