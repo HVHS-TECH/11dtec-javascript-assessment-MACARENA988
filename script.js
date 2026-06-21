4df109689cd3b8a45fd735459b4143c3b8e9bd78
MACARENA988 <24092jw@hvhs.school.nz>
2026-06-21 21:57:40 +0000

    ikjhwdqil

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ Steak = 20
 Avocado = 1
 Raw milk = 10
 ****************************/
-let userNumber = ["eggs", "steak", "avocado", "raw milk"];
+let userNumber = ["eggs", "steak", "avocadooo", "raw milk"];
 
 let price = [5, 20, 1, 10];
 /****************************
 Main code:
 ****************************/
-
+for(let i=0; i<userNumber.length; i++){
+console.log("Menu"+ i +": "+userNumber[i]+"<br>")
+}
 
 
 /****************************
@@ -71,19 +73,27 @@ function getFormInput() {
   let cost = eggsAmount * price[0] + steakAmount * price[1] + avocadoAmount * price[2] + rawmilkAmount * price[3];
 
   OUTPUT.innerHTML = "<p>Come back anytime " + name + ", we apprieciate you for ordering at Dietmaxxing.";
-  OUTPUT.innerHTML += "You ordered " +  + ".</p>";
+  OUTPUT.innerHTML += " " +  + ".</p>";
 
-  if (eggsAmount >= 1) {
-    OUTPUT.innerHTML += "You will get $" + calculateChange(money, cost) + " change.";
-  } else {
-    OUTPUT.innerHTML += "Sorry you can't afford" + ".</p>";
+  OUTPUT.innerHTML += " " +  + ".</p>";
+
+  if (eggsAmount > 0) {
+    OUTPUT.innerHTML += "Eggs:" + eggsAmount + "=" + eggsAmount * price[0];
+  }
+    if (steakAmount > 0) {
+    OUTPUT.innerHTML += "Steak:" + steakAmount + "=" + steakAmount * price[1];
+  }
+    if (avocadoAmount > 0) {
+    OUTPUT.innerHTML += "Avocado:" + avocadoAmount + "=" + avocadoAmount * price[2];
+  }
+    if (rawmilkAmount > 0) {
+    OUTPUT.innerHTML += "Raw milk:" + rawmilkAmount + "=" + rawmilkAmount * price[3];
   }
 
   if (money >= cost) {
     OUTPUT.innerHTML += "You will get $" + calculateChange(money, cost) + " change.";
-  } else {
+  } 
+  else {
     OUTPUT.innerHTML += "Sorry you can't afford" + ".</p>";
   }
-
 }
-
