03629fe3ef203acd86c1dc63fc0cb0ea81abba6b
MACARENA988 <24092jw@hvhs.school.nz>
2026-06-25 03:03:03 +0000

    Jun 25, 2026, 3:03 AM

--- script.js
+++ script.js
@@ -55,7 +55,10 @@ function viewOrder() {
   let amount = ["Number(EGGS_FIELD.value);", "Number(STEAK_FIELD.value);", " Number(AVOCADO_FIELD.value);", "Number(RAWMILK_FIELD.value);"];
   let userNumber = ["Eggs", "Steak", "Avocado", "Rawmilk"];
   let price = [5, 20, 1, 10, 77];
-  let cost = eggsAmount * price[0] + steakAmount * price[1] + avocadoAmount * price[2] + rawmilkAmount * price[3];
+
+  for (let i = 0; i < userNumber.length; i++) {
+    OUTPUT.innerHTML += (amount[i] * price[i]) + " <br>";
+  }
 
   for (let i = 0; i < userNumber.length; i++) {
     OUTPUT.innerHTML += userNumber[i] + ": " + amount[i] + " = $" + amount[i] * price[i] + " <br>";
