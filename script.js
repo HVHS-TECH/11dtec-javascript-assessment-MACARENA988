8bc81c63b394db5373c7e97dc7594b76d9051eec
MACARENA988 <24092jw@hvhs.school.nz>
2026-06-27 08:12:19 +0000

    Jun 27, 2026, 8:12 AM

--- script.js
+++ script.js
@@ -44,21 +44,17 @@ function viewOrder() {
 function getFormInput() {
   const NAME_FIELD = document.getElementById("nameField");
   const MONEY_FIELD = document.getElementById("moneyField");
-  const EGGS_FIELD = document.getElementById("eggsField");
-  const STEAK_FIELD = document.getElementById("steakField");
-  const AVOCADO_FIELD = document.getElementById("avocadoField");
-  const RAWMILK_FIELD = document.getElementById("rawmilkField");
-
   let name = NAME_FIELD.value;
   let money = Number(MONEY_FIELD.value);
 
+for (let i = 0; i < items.length; i++) {
+    const menuField = document.getElementById(items[i] + "Field")
+    let quantity = Number(menuField.value);
+    let total = quantity * prices[i]
+    cost += total
+    OUTPUT.innerHTML += items[i] + " total: $" + total + "<br>";
 
-
-  let eggsAmount = Number(EGGS_FIELD.value);
-  let steakAmount = Number(STEAK_FIELD.value);
-  let avocadoAmount = Number(AVOCADO_FIELD.value);
-  let rawmilkAmount = Number(RAWMILK_FIELD.value);
-  let cost = eggsAmount * price[0] + steakAmount * price[1] + avocadoAmount * price[2] + rawmilkAmount * price[3];
+  }
 
   if (cost > money) {
     OUTPUT.innerHTML += "Sorry, you can't afford these items.<br>";
