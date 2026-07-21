157a89255f7b421d0809a0bc0ff08a9b6f569469
MACARENA988 <24092jw@hvhs.school.nz>
2026-07-21 00:39:33 +0000

    Jul 21, 2026, 12:39 AM

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ Main code:
 
 ****************************/
 for (let i = 0; i < items.length; i++) {
-  OUTPUT.innerHTML += "$" + prices[i] + ": " + items[i] + " " + '<input placeholder="Quantity..." type="number" id="' + items[i] + 'Field" name="formMoney"  oninput="updateCart()"><br><br>';
+  OUTPUT.innerHTML += "$" + prices[i] + ": " + items[i] + " " + '<input placeholder="Quantity..." type="number" id="' + items[i] + 'Field" name="formMoney"  <img src="' + items[i] + '.jpg>" oninput="updateCart()"><br><br>';
+  console.log ("$" + prices[i] + ": " + items[i] + " " + '<input placeholder="Quantity..." type="number" id="' + items[i] + 'Field" name="formMoney"  <img src="' + items[i] + '.jpg>" oninput="updateCart()"><br><br>')
 }
 
 
